02107424dcfac3bcbf0a29cf71cf82076785290b
Kaleb <matheuskalebcruz11@gmail.com>
2025-01-15 17:08:15 -0300

    script sql de criação das tabelas no banco criado

---
+++ sql/tables.sql
@@ -0,0 +1,20 @@
+create table usuario (
+    id int not null auto_increment,
+    nome varchar(255) not null,
+    email varchar(255) not null unique,
+    senha varchar(100) not null,
+    ativo boolean not null default 1,
+    
+    primary key(id)
+);
+
+create table receita (
+    id int not null auto_increment,
+    nome varchar(255) not null,
+    descricao text not null,
+    ativo boolean not null default 1,
+    usuario_id int not null,
+    
+    primary key(id),
+    foreign key (usuario_id) references usuario(id)
+);
