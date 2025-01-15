ef8a3bafdfbf702ffac3b5403614d6805e5499f6
Kaleb <matheuskalebcruz11@gmail.com>
2025-01-15 17:06:36 -0300

    dependencia do mysql connector e dotenv adicionados no projeto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module br.ifpr.edu.receitas {
     requires javafx.controls;
     requires javafx.fxml;
+    
+    requires io.github.cdimascio.dotenv.java;
+    requires java.sql;
 
     opens br.ifpr.edu.receitas.screens to javafx.fxml;
     exports br.ifpr.edu.receitas;
